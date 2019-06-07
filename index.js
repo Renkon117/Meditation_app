4198a01344282f82cba4466f0194b0fa759dad9b
okada001 <okada001@rangers.uwp.edu>
2019-06-07 17:27:28 -0500

    connected to api

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require  ("express");
+const api = express ();
+
+api.use(express.static("./"));
+
+const PORT = process.env.PORT || 3001
+// localhost:PORT
+api.listen(PORT, () => {
+    console.log("Server is up and listening on: " + PORT);
+});
+  
+
+
+api.get("/index.html",(req, res) => {
+    console.log("Responding to root route")
+    res.send("Hello world")
+});
+
